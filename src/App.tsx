69dbac34bbfbb38b4412856ea1e9bb2b72196323
Edwin Hernández <edwinhdevelop@gmail.com>
2024-05-02 07:48:49 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@nextui-org/react"
+
+function App() {
+
+  return (
+    <main>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button
+        color="primary"
+      >Test</Button>
+    </main>
+  )
+}
+
+export default App
